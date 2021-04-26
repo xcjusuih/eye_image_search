e744dc46d08d08093f8e583d23e53f7e3a352d1d
xcjusuih <xcjusuih@gmail.com>
2021-04-26 15:23:45 +0800

    Create database.py

---
+++ database.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+class Database:
+
+    def __init__(self,database,user,password,host,port):
+        self.conn=psycopg2.connect(database=database,user=user,password=password,host=host,port=port)
+        self.cur=self.conn.cursor()
+    
+    def close(self):
+        self.cur.close()
+        self.conn.close()
+    
+    def query(self,filename):
+        self.cur.execute('select * from patient where filename=%s',('IM_091715.png',))
+        return self.cur.fetchall()    
+
+database=Database("postgres","postgres","123456","10.17.12.166","5432")
+print(database.query("IM_091715.png"))
+database.close()
+
+
